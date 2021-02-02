d3c1dd687511bdb41083ae3b17ac05d1b06e37be
Joseph <40442074@live.napier.ac.uk>
2021-02-02 12:02:55 +0000

    Update main.cpp
    Added initial variables and added load function

--- pong/main.cpp
+++ pong/main.cpp
@@ -1,5 +1,38 @@
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+using namespace std;
+
+const Keyboard::Key controls[4] = {
+    Keyboard::A,
+    Keyboard::Z,
+    Keyboard::Up,
+    Keyboard::Down
+};
+const Vector2f paddleSize(25.0f, 100.0f);
+const float ballRadius = 10.0f;
+const int gameWidth = 800;
+const int gameHeight = 600;
+const float paddleSpeed = 400.0f;
+
+CircleShape ball;
+RectangleShape paddles[2];
+
+void Load() {
+    for (auto& p : paddles) {
+        p.setSize(paddleSize - Vector2f(3, 3));
+        p.setOrigin(paddleSize / 2.0f);
+    }
+
+    ball.setRadius(ballRadius - 3);
+    ball.setOrigin(ballRadius / 2, ballRadius / 2);
+
+    paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
+    paddles[1].setPosition(-10 - paddleSize.x / 2, gameHeight / 2);
+
+    ball.setPosition(gameWidth / 2, gameHeight / 2);
+}
+
 int main(){
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.0f);
