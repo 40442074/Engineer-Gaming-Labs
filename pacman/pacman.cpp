a654e9bea7ad7ff038da7a8697ad6dabd0d84dba
Joseph <40442074@live.napier.ac.uk>
2021-05-04 13:06:23 +0100

    implemented menu scene

--- pacman/pacman.cpp
+++ pacman/pacman.cpp
@@ -1,5 +1,29 @@
 #include "pacman.h"
+#include "Windows.h"
+using namespace sf;
+using namespace std;
 
-std::shared_ptr<Scene> gameScene;
-std::shared_ptr<Scene> menuScene;
-std::shared_ptr<Scene> activeScene;
+shared_ptr<Scene> gameScene;
+shared_ptr<Scene> menuScene;
+shared_ptr<Scene> activeScene;
+
+void MenuScene::update(double dt) {
+	Scene::update(dt);
+	text.setString("Almost Pacman");
+}
+
+void MenuScene::render() {
+	Renderer::queue(&text);
+	Scene::render();
+}
+
+void MenuScene::load() {
+	char c[MAX_PATH];
+	GetModuleFileName(NULL, c, MAX_PATH);
+	string s = string(c);
+	s = s.substr(0, s.find_last_of("\\/"));
+
+	Font f = Font();
+	f.loadFromFile(s + "\\res\\font.txt");
+	text = Text("Almost Pacman", f);
+}
