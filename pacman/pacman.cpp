bce1c2054356a2062fd570f59e008ca078fe6b1a
Joseph <40442074@live.napier.ac.uk>
2021-05-04 12:40:41 +0100

    Implemented scene management

---
+++ pacman/pacman.cpp
@@ -0,0 +1,5 @@
+#include "pacman.h"
+
+std::shared_ptr<Scene> gameScene;
+std::shared_ptr<Scene> menuScene;
+std::shared_ptr<Scene> activeScene;
---
+++ pacman/scene.cpp
@@ -0,0 +1,9 @@
+#include "scene.h"
+
+void Scene::update(double dt) {
+	_ents.update(dt);
+}
+
+void Scene::render() {
+	_ents.render();
+}
